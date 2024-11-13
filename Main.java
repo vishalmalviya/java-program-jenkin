8bab28f0e178b57cca0712a272f639749e24fa0d
vishalmalviya <visu.malviya@gmail.com>
2024-11-13 08:35:07 +0530

    Init commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String ...s){
         System.out.println("This is java program by vishal on date "+new Date());
+        System.out.println("This is new change to push");
     }
 }
