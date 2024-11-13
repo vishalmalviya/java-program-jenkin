246e76df5376f3327a61b6d8575ba9dccbc2c80b
vishalmalviya <visu.malviya@gmail.com>
2024-11-13 08:57:59 +0530

    Added student class to check jenkin build futher

--- Main.java
+++ Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("This is java program by vishal on date "+new Date());
         System.out.println("This is new change to push");
         System.out.println("Adding one more line to check jenkin auto build");
+
+       Student student = new Student();
+       student.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating student object.");
+    }
+
+    public void sayHello(){
+        System.out.println("Hello, I am student.");
+    }
+
+}
