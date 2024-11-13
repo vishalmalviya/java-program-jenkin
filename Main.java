00641d80366dd9dc915f827b2647766e1b19a95a
vishalmalviya <visu.malviya@gmail.com>
2024-11-13 08:49:27 +0530

    Auto trigger jenkin build

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String ...s){
         System.out.println("This is java program by vishal on date "+new Date());
         System.out.println("This is new change to push");
+        System.out.println("Adding one more line to check jenkin auto build");
     }
 }
